1b53e8d47dd99ae4547f5dbbe102703bbf64b3d2
ipomoney-admin <sahib13singh13@gmail.com>
2026-02-07 21:23:56 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -8,7 +8,7 @@ import os
 URL = "https://khnuyrhafzppbugebjdn.supabase.co"
 KEY = "sb_secret_SIb_8imA5DxLxNVK1srMDQ__xLolWEV"
 # Google Sheet CSV Link (Niche bataya hai kaise milega)
-SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGp-0ECuXqbLmlDHfvKbfu0IqfFxqwnnEpA376aco74HqOuctesu17dUWPgYPVPkx6xUFS51RqhzmM/pubhtml"
+SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGp-0ECuXqbLmlDHfvKbfu0IqfFxqwnnEpA376aco74HqOuctesu17dUWPgYPVPkx6xUFS51RqhzmM/pub?output=csv"
 
 supabase = create_client(URL, KEY)
 
